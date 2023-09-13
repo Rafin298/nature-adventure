1d1bae5bc31152b5aa523f26afb864a7a7fa6fe1
Rafin298 <rafinkhan298@gmail.com>
2023-09-13 10:33:50 +0600

    fourth commit

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const helmet = require('helmet');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
-// Set security HTTP headers 
-app.use(helmet());
-
-
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
